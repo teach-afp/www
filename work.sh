51b183b908e37a84cc8cb719b36e87290ec7431a
Alejandro Russo <russo@chalmers.se>
2016-01-05 16:47:45 +0100

    Added page.html in the watch

--- work.sh
+++ work.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #https://facebook.github.io/watchman/docs/watchman-make.html
-watchman-make --make './make_web.sh' -p '**/*.md' '**/GNUmakefile' -t all
+watchman-make --make './make_web.sh' -p '**/*.md' '**/page.html' '**/GNUmakefile' -t all
